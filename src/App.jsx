b31c77c30ecb2ed5cf49528a164c1b48fcdeffcc
Xpression <emmybanks03@gmail.com>
2025-01-25 16:39:13 +0100

    add functionality to toggle button state and track item count in cart

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import CartCount from './Cartcount';
 
 function App() {
   const [cartCount, setCount] = useState(0);
+  const [isClicked, setIsClciked] = useState(false);
+  const [itemCount, setItemCount] = useState(0);
   const waffles = {
     "image": {
             "thumbnail": ".src/assets/images/image-waffle-thumbnail.jpg",
@@ -107,6 +109,30 @@ const vanillaPannaCotta = {
   "price": 6.50
 };
 
+const handleClick = ()=>{
+  setIsClciked((prev) => !prev);
+  setCount((c)=> c + 1)
+}
+const handleCount = ()=>{
+  setItemCount((c)=> c + 1)
+}
+const button ={
+  backgroundColor: isClicked? 'red' : '',
+  color: isClicked? 'white' : '',
+  borderRadius: isClicked? '28px' : '',
+    textAlign: 'center',
+    padding: isClicked? '12px' : '',
+    zIndex: isClicked? '-2px' : ''
+}
+const decrement = {
+  position: 'absolute',
+  left: 0
+}
+const increment = {
+  position: 'absolute',
+  right: 0,
+  zIndex: '20px'
+}
   
   return (
     <>
@@ -118,7 +144,12 @@ const vanillaPannaCotta = {
         <div className='dessert waffles'>
         <span style={{position: 'relative'}}>
           <img src={waffles.image.desktop}/>
-          <button onClick={()=> setCount((c)=> c + 1)} className='add'><svg xmlns="http://www.w3.org/2000/svg" width="11" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clipPath="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>Add to cart</button>
+          <button onClick={handleClick} className='add' style={button}>
+           {isClicked ? 
+           <svg style={decrement} xmlns="http://www.w3.org/2000/svg" width="20" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>: <span><svg xmlns="http://www.w3.org/2000/svg" width="11" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clipPath="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>Add to cart</span>}
+           {isClicked? <svg  style={increment} onClick={handleCount} xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg> : ''}
+           {itemCount}
+           </button>
           </span>
           <p>{waffles.category}</p>
           <p>{waffles.name}</p>
